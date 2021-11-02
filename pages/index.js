a8baae3deb41de16d7360c9f04c7ce3c7f1da37c
Chris Hannaby <chris.hannaby@netlify.com>
2021-11-01 19:05:30 -0600

    asplit odb pages into critical and deferred

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import staticPageSlugs from "../lib/staticPages.preval";
+import odbPageSlugs from "../lib/odbPages.preval";
 import { generateSlug } from "random-word-slugs";
 
 export async function getStaticProps() {
@@ -30,7 +31,7 @@ export default function Home({ staticPages, nonExistingStaticSlug }) {
             return (
               <li key={page}>
                 <Link href={`/static-only/${page}`}>
-                  <a>{page}</a>
+                  <a>{`/static-only/${page}`}</a>
                 </Link>
               </li>
             );
@@ -38,14 +39,14 @@ export default function Home({ staticPages, nonExistingStaticSlug }) {
         </ul>
         <p>
           For these pages we have set <code>fallback: false</code> so if we try
-          to vist a random slug that was not returned from{" "}
+          to vist a any slug that was not returned from{" "}
           <code>getStaticPaths</code> we will see 404 error.
         </p>
         <p>
           {" "}
           Try visiting{" "}
           <Link href={`/static-only/${nonExistingStaticSlug}`}>
-            <a>{nonExistingStaticSlug}</a>
+            <a>{`/static-only/${nonExistingStaticSlug}`}</a>
           </Link>
           .
         </p>
@@ -55,7 +56,7 @@ export default function Home({ staticPages, nonExistingStaticSlug }) {
           so they will be statically generated as part of our build.
         </p>
         <ul>
-          {staticPageSlugs.map((page) => {
+          {odbPageSlugs.critical.map((page) => {
             const path = `/odb/${page}`;
             return (
               <li key={page}>
@@ -69,16 +70,22 @@ export default function Home({ staticPages, nonExistingStaticSlug }) {
         <p>
           For this directory we have set{" "}
           <code>fallback: &quot;blocking&quot;</code> so if we try to vist a
-          random slug that was not returned from <code>getStaticPaths</code> it
-          will be build on-demand.
+          valid slug that was not returned from <code>getStaticPaths</code> it
+          will be build on-demand and then cached for future visitors. You can
+          test this at the links below:
         </p>
-        <p>
-          Try visiting{" "}
-          <Link href={`/odb/${nonExistingStaticSlug}`}>
-            <a>{`/odb/${nonExistingStaticSlug}`}</a>
-          </Link>{" "}
-          or any other random slug .
-        </p>
+        <ul>
+          {odbPageSlugs.deferred.map((page) => {
+            const path = `/odb/${page}`;
+            return (
+              <li key={page}>
+                <Link href={path}>
+                  <a>{path}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
         <h2>Server Side Rendered</h2>
         <p>
           The following page will be server side rendered and fetch new data on
@@ -89,6 +96,45 @@ export default function Home({ staticPages, nonExistingStaticSlug }) {
             <Link href="/ssr">
               <a>/ssr</a>
             </Link>
+          </li>
+        </ul>
+        <h2>ISR</h2>
+        <p>
+          Netlify does not support <code>stale-while-revalidate</code> which is
+          a caching mechanism that is used to power ISR. You can read some of
+          the reasons why this is not supported{" "}
+          <a href="https://www.netlify.com/blog/2021/03/08/incremental-static-regeneration-its-benefits-and-its-flaws/">
+            here
+          </a>
+          . As a fallback any page that uses <code>revalidate</code> will be
+          server side rendered and fetch new data on every request. See an
+          example here:
+        </p>
+        <ul>
+          <li>
+            <Link href="/isr">
+              <a>/isr</a>
+            </Link>
+          </li>
+        </ul>
+        <p>
+          This is only meant as a fallback. We recommend that you try to use one
+          of the following approaches instead:
+        </p>
+        <ul>
+          <li>
+            <strong>For moderate numbers of pages (100s):</strong> statically
+            generate all pages and trigger a rebuild when data/content changes.
+          </li>
+          <li>
+            <strong>For very large numbers of pages (1000s):</strong> statically
+            generate the most popular pages and build the rest on demand.
+          </li>
+          <li>
+            <strong>For pages with a mix of static and dynamic content:</strong>{" "}
+            statically generate the page including all of the relatively static
+            content and fetch more dynamic content (prices, availability etc.
+            client side).
           </li>
         </ul>
       </section>
--- pages/isr.js
+++ pages/isr.js
@@ -1,16 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import odbPageSlugs from "../../lib/odbPages.preval";
-import siteBuiltAt from "../../lib/buildTime.preval";
-import { formatDate } from "../../lib/formatDate";
+import siteBuiltAt from "../lib/buildTime.preval";
+import { formatDate } from "../lib/formatDate";
 
-export async function getStaticProps({ params }) {
-  if (!odbPageSlugs.all.includes(params.slug)) {
-    return {
-      notFound: true,
-    };
-  }
-  const isCritical = odbPageSlugs.critical.includes(params.slug);
+export async function getStaticProps() {
   const pageBuildAt = formatDate(new Date());
   const hero = await fetch("https://source.unsplash.com/random/700x300");
   const heroSrc = await hero.url;
@@ -20,46 +13,26 @@ export async function getStaticProps({ params }) {
   const html = await randomContent.text();
   return {
     props: {
-      title: params.slug,
       content: html,
       heroSrc,
       pageBuildAt,
       siteBuiltAt,
-      isCritical,
     },
+    revalidate: 60, // setting revalidate to any value will cause Netlify to fall back to SSR for this page
   };
 }
 
-export async function getStaticPaths() {
-  // Just generate the critical pages at build time
-  const paths = odbPageSlugs.critical.map((slug) => {
-    return {
-      params: {
-        slug,
-      },
-    };
-  });
-  return { paths, fallback: "blocking" };
-}
-
-export default function Post({
-  title,
-  content,
-  heroSrc,
-  pageBuildAt,
-  siteBuiltAt,
-  isCritical,
-}) {
+export default function Post({ content, heroSrc, pageBuildAt, siteBuiltAt }) {
   return (
     <>
       <Head>
-        <title>Next.js | {title}</title>
+        <title>Next.js | ISR</title>
       </Head>
       <div className="text-lg max-w-prose mx-auto h-72 relative">
         <div className="absolute bg-gray-600 w-full h-full opacity-60 top-0 left-0 z-10"></div>
         <div className="absolute z-20 flex flex-col items-center w-full justify-center h-full">
           <div className="text-white font-bold text-6xl overflow-ellipsis overflow-hidden">
-            {title}
+            Incremental Static Regeneration
           </div>
         </div>
         <Image
@@ -71,17 +44,7 @@ export default function Post({
         />
       </div>
       <div className="mt-12 prose">
-        {isCritical ? (
-          <h2>Critical page generated at build time</h2>
-        ) : (
-          <h2>
-            Deferred page generated on-demand via an{" "}
-            <a href="https://docs.netlify.com/configure-builds/on-demand-builders/">
-              On-demand builder
-            </a>
-          </h2>
-        )}
-
+        <h2>ISR Page</h2>
         <p>
           Site build started at: <strong>{siteBuiltAt}</strong>
         </p>
